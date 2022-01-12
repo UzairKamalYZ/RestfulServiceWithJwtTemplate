249d3b93e39aceeb1326e657e22955797448ab72
ukamal <ukamal@isabelgroup.eu>
2022-01-12 11:28:29 +0100

    chore(typo): correction

--- src/main/java/be/techfix/template/rest/Hello.java
+++ src/main/java/be/techfix/template/rest/Hello.java
@@ -1,13 +1,14 @@
 package be.techfix.template.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hello {
 
     @GetMapping("/hello")
-    public String hello(){
-        return "hello dear user";
+    public String hello(@RequestParam("user") String user){
+        return "hello  "+user;
     }
 }
